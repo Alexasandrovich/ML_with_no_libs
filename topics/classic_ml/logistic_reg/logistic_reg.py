ec99acb69b9aada3d78a157acbca1ba8214851cb
aleksandrars <aleksandrars@hflabs.ru>
2025-01-17 00:01:04 +0300

    fixes

--- topics/classic_ml/logistic_reg/logistic_reg.py
+++ topics/classic_ml/logistic_reg/logistic_reg.py
@@ -17,30 +17,30 @@ class MyLogReg:
         X.insert(0, '', 1.0)
         feature_size = X.shape[1]
         self.weights = np.ones((feature_size, 1))  # init
-        for i in range(self.n_iter):
+        for index in range(self.n_iter):
             # predict
-            y_pred = X.dot(self.weights)
+            y_pred = 1 / (1 + np.exp(-X.dot(self.weights)))
 
             # evaluate loss and grad
             loss = self.calc_loss(y_pred, y)
-            grad = self.calc_grad(y_pred, y)
+            grad = self.calc_grad(y_pred, y, X)
 
             # update step
-            self.weights = self.weights - self.learning_rate * grad
+            self.weights = self.weights - self.learning_rate * grad.values
             if verbose and index % verbose == 0:
-                basic_log = f"{'start' if index == 0 else index} | loss: {loss} | lr: {current_learning_rate}"
+                basic_log = f"{'start' if index == 0 else index} | loss: {loss} | lr: {self.learning_rate}"
                 print(basic_log)
 
 
     def calc_loss(self, y_pred, y_gt, eps=1e-8):
         p = 1 / (1 + np.exp(-y_pred))
-        return -np.mean(y_gt * np.log(p + eps) + (1 - y) * np.log(1 - p + eps))
+        return -np.mean(y_gt * np.log(p + eps) + (1 - y_gt) * np.log(1 - p + eps))
 
-    def calc_grad(self, y_pred, y_gt):
-        return 1 / y_pred.shape[0] * (y_pred.values - y_gt.values.reshape(-1, 1)).T.dot(X.values)
+    def calc_grad(self, p, y_gt, X):
+        return X.T.dot(p - y_gt.values.reshape(-1, 1)) / X.shape[0]
 
     def get_coef(self):
-        return self.weights
+        return self.weights[1:]
 
 
 def generate_dataset(n_samples=200, n_features=3, random_state=42):
@@ -58,5 +58,5 @@ def generate_dataset(n_samples=200, n_features=3, random_state=42):
 
 X, y = generate_dataset(n_samples=200, n_features=3, random_state=42)
 logreg = MyLogReg()
-logreg.fit(X, y)
-print(logreg.get_coeffs())
+logreg.fit(X, y, 1)
+print(logreg.get_coef())
