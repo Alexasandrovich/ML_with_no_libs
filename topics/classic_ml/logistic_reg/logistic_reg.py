7adc203158213d3875bcc6b01481532f00faf32e
aleksandrars <aleksandrars@hflabs.ru>
2025-01-18 17:08:39 +0300

    fixes

--- topics/classic_ml/logistic_reg/logistic_reg.py
+++ topics/classic_ml/logistic_reg/logistic_reg.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import random
 from sklearn.datasets import make_classification
 
 
@@ -14,30 +13,27 @@ class MyLogReg:
         return f"MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
 
     def fit(self, X: pd.DataFrame, y: pd.Series, verbose=0):
-        X.insert(0, '', 1.0)
+        X = X.copy()
+        X.insert(0, 'bias', 1.0)
         feature_size = X.shape[1]
-        self.weights = np.ones((feature_size, 1))  # init
+        self.weights = np.ones((feature_size, 1))
+        y = y.to_numpy().reshape(-1, 1)
+
         for index in range(self.n_iter):
-            # predict
-            y_pred = 1 / (1 + np.exp(-X.dot(self.weights)))
+            y_pred = 1 / (1 + np.exp(-X.to_numpy().dot(self.weights)))
 
-            # evaluate loss and grad
             loss = self.calc_loss(y_pred, y)
-            grad = self.calc_grad(y_pred, y, X)
+            grad = self.calc_grad(y_pred, y, X.to_numpy())
 
-            # update step
-            self.weights = self.weights - self.learning_rate * grad.values
+            self.weights -= self.learning_rate * grad
             if verbose and index % verbose == 0:
-                basic_log = f"{'start' if index == 0 else index} | loss: {loss} | lr: {self.learning_rate}"
-                print(basic_log)
-
+                print(f"Iter {index} | Loss: {loss:.4f} | LR: {self.learning_rate}")
 
     def calc_loss(self, y_pred, y_gt, eps=1e-8):
-        p = 1 / (1 + np.exp(-y_pred))
-        return -np.mean(y_gt * np.log(p + eps) + (1 - y_gt) * np.log(1 - p + eps))
+        return -np.mean(y_gt * np.log(y_pred + eps) + (1 - y_gt) * np.log(1 - y_pred + eps))
 
     def calc_grad(self, p, y_gt, X):
-        return X.T.dot(p - y_gt.values.reshape(-1, 1)) / X.shape[0]
+        return (X.T.dot(p - y_gt)) / X.shape[0]
 
     def get_coef(self):
         return self.weights[1:]
@@ -56,7 +52,9 @@ def generate_dataset(n_samples=200, n_features=3, random_state=42):
     y = pd.Series(y, name="target")
     return X, y
 
-X, y = generate_dataset(n_samples=200, n_features=3, random_state=42)
-logreg = MyLogReg()
-logreg.fit(X, y, 1)
-print(logreg.get_coef())
+
+# Генерация данных и обучение
+X, y = generate_dataset(n_samples=200, n_features=2, random_state=42)
+logreg = MyLogReg(n_iter=100, learning_rate=0.1)
+logreg.fit(X, y, verbose=10)
+print("Coefficients:\n", logreg.get_coef())
