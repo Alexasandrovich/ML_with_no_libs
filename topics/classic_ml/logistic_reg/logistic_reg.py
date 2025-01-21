e28223e56dcd1f4d2e5d860a510221c6a24eab33
aleksandrars <aleksandrars@hflabs.ru>
2025-01-21 22:33:19 +0300

    add some metrics

--- topics/classic_ml/logistic_reg/logistic_reg.py
+++ topics/classic_ml/logistic_reg/logistic_reg.py
@@ -4,10 +4,11 @@ from sklearn.datasets import make_classification
 
 
 class MyLogReg:
-    def __init__(self, n_iter=10, learning_rate=0.1):
+    def __init__(self, n_iter=10, learning_rate=0.1, metric=None):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = None
+        self.metric_type = metric
 
     def __repr__(self):
         return f"MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
@@ -20,14 +21,53 @@ class MyLogReg:
         y = y.to_numpy().reshape(-1, 1)
 
         for index in range(self.n_iter):
-            y_pred = 1 / (1 + np.exp(-X.to_numpy().dot(self.weights)))
+            y_pred = self.apply_sigmoid(X)
 
             loss = self.calc_loss(y_pred, y)
             grad = self.calc_grad(y_pred, y, X.to_numpy())
 
             self.weights -= self.learning_rate * grad
+            self.metric = self.calc_metric(y, y_pred, self.metric_type)
             if verbose and index % verbose == 0:
-                print(f"Iter {index} | Loss: {loss:.4f} | LR: {self.learning_rate}")
+                print(f"Iter {index} | Loss: {loss:.4f} | LR: {self.learning_rate} | metric({self.metric_type}): {self.metric:.2f}")
+
+    def predict(self, X: pd.DataFrame):
+        X_preprocessed = X.copy()
+        X_preprocessed.insert(0, 'w0', 1.0)
+        preds = self.apply_sigmoid(X_preprocessed)
+        return preds > 0.5
+
+    def get_best_metric(self):
+        return self.metric
+
+    def calc_metric(self, y, y_pred, metric_type):
+        tp = np.sum((y == 1) & (y_pred >= 0.5))
+        tn = np.sum((y == 0) & (y_pred < 0.5))
+        fp = np.sum((y == 0) & (y_pred >= 0.5))
+        fn = np.sum((y == 1) & (y_pred < 0.5))
+        if metric_type == "accuracy":
+            return (tp + tn) / (tp + tn + fp + fn)
+        elif metric_type == "precision":
+            return tp / (tp + fp)
+        elif metric_type == "recall":
+            return tp / (tp + fn)
+        elif metric_type == "f1":
+            recall = tp / (tp + fn)
+            precision = tp / (tp + fp)
+            return 2 * (recall * precision) / (recall + precision)
+        elif metric_type == "roc_auc":
+            return 0.0
+        else:
+            return None
+
+    def predict_proba(self, X: pd.DataFrame):
+        X_preprocessed = X.copy()
+        X_preprocessed.insert(0, 'w0', 1.0)
+        preds = self.apply_sigmoid(X_preprocessed)
+        return preds
+
+    def apply_sigmoid(self, X):
+        return 1 / (1 + np.exp(-X.to_numpy().dot(self.weights)))
 
     def calc_loss(self, y_pred, y_gt, eps=1e-8):
         return -np.mean(y_gt * np.log(y_pred + eps) + (1 - y_gt) * np.log(1 - y_pred + eps))
@@ -53,8 +93,7 @@ def generate_dataset(n_samples=200, n_features=3, random_state=42):
     return X, y
 
 
-# Генерация данных и обучение
 X, y = generate_dataset(n_samples=200, n_features=2, random_state=42)
-logreg = MyLogReg(n_iter=100, learning_rate=0.1)
+logreg = MyLogReg(n_iter=100, learning_rate=0.1, metric="f1")
 logreg.fit(X, y, verbose=10)
-print("Coefficients:\n", logreg.get_coef())
+logreg_pred = logreg.predict(X)
